12e8cd00d420e1c1f71627a2aca70f59bdfac6c1
wonderwallz <54917198+wonderwallz@users.noreply.github.com>
2019-10-13 23:57:29 -0700

    Q5

---
+++ Assignment5/Boardable.java
@@ -0,0 +1,8 @@
+package assignment5;
+
+public interface Boardable {
+	public void setBoardStart(int month, int day, int year);
+	public void setBoardEnd(int month, int day, int year);
+	public boolean boarding(int month, int day, int year);
+
+}
