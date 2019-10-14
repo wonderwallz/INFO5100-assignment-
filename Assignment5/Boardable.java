9e9bf06c9a316f72446cf01ceead158bec7cbf57
wonderwallz <54917198+wonderwallz@users.noreply.github.com>
2019-10-13 23:59:38 -0700

    Delete Boardable.java

--- Assignment5/Boardable.java
+++
@@ -1,8 +0,0 @@
-package assignment5;
-
-public interface Boardable {
-	public void setBoardStart(int month, int day, int year);
-	public void setBoardEnd(int month, int day, int year);
-	public boolean boarding(int month, int day, int year);
-
-}
